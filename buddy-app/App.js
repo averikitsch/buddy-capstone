f087e6d4b45741052ccde016ddfb4e27b96920c7
averikitsch <aekitsch@gmail.com>
2018-01-02 12:27:50 -0800

    added and installed dependencies

--- buddy-app/App.js
+++ buddy-app/App.js
@@ -1,5 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, View } from 'react-native';
+import { Provider, connect } from 'react-redux';
+import { Font, AppLoading } from 'expo';
+import store from './app/redux/store';
+import Router from './app/config/routes';
+// AWS IMPORTS
+import Amplify from 'aws-amplify-react-native';
+import aws_exports from './aws-exports';
+
+Amplify.configure(aws_exports);
 
 export default class App extends React.Component {
   render() {
