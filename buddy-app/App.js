a112acb227a3afa10a9523ccdee3c7242eec060d
averikitsch <aekitsch@gmail.com>
2018-01-02 13:25:39 -0800

    created new app with react-native

--- buddy-app/App.js
+++ buddy-app/App.js
@@ -10,13 +10,23 @@ import aws_exports from './aws-exports';
 
 Amplify.configure(aws_exports);
 
+// async componentWillMount() {
+//   await Font.loadAsync({
+//     'Roboto': require('native-base/Fonts/Roboto.ttf'),
+//     'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+//     'Pacifico': require('./app/assets/fonts/Pacifico.ttf'),
+//     'Ionicons': require('native-base/Fonts/Ionicons.ttf'),
+//   });
+// }
+
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Changes you make will automatically reload.</Text>
+      <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
