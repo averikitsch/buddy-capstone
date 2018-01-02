6f4d5f78b8ace8346bf75164af2078c407e5f43c
averikitsch <aekitsch@gmail.com>
2018-01-02 14:53:29 -0800

    working tabs

--- buddyApp/App.js
+++ buddyApp/App.js
@@ -4,54 +4,100 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+// import React, { Component } from 'react';
+// import {
+//   Platform,
+//   StyleSheet,
+//   Text,
+//   View
+// } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' +
+//     'Cmd+D or shake for dev menu',
+//   android: 'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// });
+//
+// export default class App extends Component<{}> {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text style={styles.welcome}>
+//           Welcome to React Native!
+//         </Text>
+//         <Text style={styles.instructions}>
+//           To get started, edit App.js
+//         </Text>
+//         <Text style={styles.instructions}>
+//           {instructions}
+//         </Text>
+//       </View>
+//     );
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
 
-export default class App extends Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
+import React, { Component } from "react";
+import LucyChat from "./src/LucyChat.js";
+import JadeChat from "./src/JadeChat.js";
+import NineChat from "./src/NineChat.js";
+import { TabNavigator } from "react-navigation";
+import { Button, Text, Icon, Footer, FooterTab } from "native-base";
+export default (MainScreenNavigator = TabNavigator(
+  {
+    LucyChat: { screen: LucyChat },
+    JadeChat: { screen: JadeChat },
+    NineChat: { screen: NineChat }
+  },
+  {
+    tabBarPosition: "bottom",
+    tabBarComponent: props => {
+      return (
+        <Footer>
+          <FooterTab>
+            <Button
+              vertical
+              active={props.navigationState.index === 0}
+              onPress={() => props.navigation.navigate("LucyChat")}>
+              <Icon name="bowtie" />
+              <Text>Lucy</Text>
+            </Button>
+            <Button
+              vertical
+              active={props.navigationState.index === 1}
+              onPress={() => props.navigation.navigate("JadeChat")}>
+              <Icon name="briefcase" />
+              <Text>Jade</Text>
+            </Button>
+            <Button
+              vertical
+              active={props.navigationState.index === 2}
+              onPress={() => props.navigation.navigate("NineChat")}>
+              <Icon name="headset" />
+              <Text>Nine</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      );
+    }
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+));
---
+++ buddyApp/src/JadeChat.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { AppRegistry, View, StatusBar } from "react-native";
+import { Container, Body, Content, Header, Left, Right, Icon, Title, Input, Item, Label, Button, Text } from "native-base";
+
+export default class JadeChat extends React.Component {
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <Container>
+        <Header>
+          <Left>
+            <Button
+              transparent
+              onPress={() => this.props.navigation.navigate("DrawerOpen")}>
+              <Icon name="menu" />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Jade Chat</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content padder>
+          <Item floatingLabel style={{ marginTop: 20 }}>
+            <Label>Jade Chat</Label>
+            <Input />
+          </Item>
+          <Button rounded danger
+            style={{ marginTop: 20, alignSelf: "center" }}
+            onPress={() => navigate("Profile")}>
+            <Text>Goto Jade Profile</Text>
+          </Button>
+        </Content>
+      </Container>
+    );
+  }
+}
