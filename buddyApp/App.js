6d55a0d63770a9f80963314c6cef085dae676639
averikitsch <aekitsch@gmail.com>
2018-01-02 16:26:41 -0800

    renamed tabs and changed icons

--- buddyApp/App.js
+++ buddyApp/App.js
@@ -1,72 +1,16 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-// import React, { Component } from 'react';
-// import {
-//   Platform,
-//   StyleSheet,
-//   Text,
-//   View
-// } from 'react-native';
-
-// const instructions = Platform.select({
-//   ios: 'Press Cmd+R to reload,\n' +
-//     'Cmd+D or shake for dev menu',
-//   android: 'Double tap R on your keyboard to reload,\n' +
-//     'Shake or press menu button for dev menu',
-// });
-//
-// export default class App extends Component<{}> {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text style={styles.welcome}>
-//           Welcome to React Native!
-//         </Text>
-//         <Text style={styles.instructions}>
-//           To get started, edit App.js
-//         </Text>
-//         <Text style={styles.instructions}>
-//           {instructions}
-//         </Text>
-//       </View>
-//     );
-//   }
-// }
-//
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-// });
-
+/* eslint-disable */
 import React, { Component } from "react";
-import LucyChat from "./src/LucyChat.js";
-import JadeChat from "./src/JadeChat.js";
-import NineChat from "./src/NineChat.js";
+import ExploreChat from "./src/ExploreScreen.js";
+import SearchChat from "./src/SearchScreen.js";
+import ProfileChat from "./src/ProfileScreen.js";
 import { TabNavigator } from "react-navigation";
-import { Button, Text, Icon, Footer, FooterTab } from "native-base";
+import { Button, Text, Footer, FooterTab } from "native-base";
+import Icon from 'react-native-vector-icons/Ionicons';
 export default (MainScreenNavigator = TabNavigator(
   {
-    LucyChat: { screen: LucyChat },
-    JadeChat: { screen: JadeChat },
-    NineChat: { screen: NineChat }
+    ExploreChat: { screen: ExploreChat },
+    SearchChat: { screen: SearchChat },
+    ProfileChat: { screen: ProfileChat }
   },
   {
     tabBarPosition: "bottom",
@@ -77,23 +21,23 @@ export default (MainScreenNavigator = TabNavigator(
             <Button
               vertical
               active={props.navigationState.index === 0}
-              onPress={() => props.navigation.navigate("LucyChat")}>
-              <Icon name="bowtie" />
-              <Text>Lucy</Text>
+              onPress={() => props.navigation.navigate("ExploreChat")}>
+              <Icon name="ios-compass" size={30} />
+
             </Button>
             <Button
               vertical
               active={props.navigationState.index === 1}
-              onPress={() => props.navigation.navigate("JadeChat")}>
-              <Icon name="briefcase" />
-              <Text>Jade</Text>
+              onPress={() => props.navigation.navigate("SearchChat")}>
+              <Icon name="ios-camera" size={35} />
+
             </Button>
             <Button
               vertical
               active={props.navigationState.index === 2}
-              onPress={() => props.navigation.navigate("NineChat")}>
-              <Icon name="headset" />
-              <Text>Nine</Text>
+              onPress={() => props.navigation.navigate("ProfileChat")}>
+              <Icon name="md-contact" size={30} />
+
             </Button>
           </FooterTab>
         </Footer>
--- buddyApp/src/SearchScreen.js
+++ buddyApp/src/SearchScreen.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from "react";
 import { AppRegistry, View, StatusBar } from "react-native";
 import { Container, Body, Content, Header, Left, Right, Icon, Title, Input, Item, Label, Button, Text } from "native-base";
