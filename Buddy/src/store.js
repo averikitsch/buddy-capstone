0066c9c9f8965c0b52032a470d3727bb9d73c859
averikitsch <aekitsch@gmail.com>
2018-01-10 20:46:19 -0800

    tested add log function

--- Buddy/src/store.js
+++ Buddy/src/store.js
@@ -1,9 +1,14 @@
 import { createStore } from 'redux';
-import reducer from './Reducers';
-
+import reducer from './Reducers/';
+import { addLog } from './Actions/index'
 
 const store = createStore(reducer);
+
 store.subscribe(() => {
   console.log("store changed", store.getState())
 })
+
+// store.dispatch(addLog({name: 'fake', brand:'fakey', product: 'hi'}))
+
+
 export default store;
