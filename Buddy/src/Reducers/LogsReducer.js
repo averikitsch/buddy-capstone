a44d49bad7ab44711ffca332adc780a7850a7b9e
averikitsch <aekitsch@gmail.com>
2018-01-10 20:47:39 -0800

    updating initial list

--- Buddy/src/Reducers/LogsReducer.js
+++ Buddy/src/Reducers/LogsReducer.js
@@ -1,19 +1,20 @@
 export default function reducer(state={
   logs: [
-      {id: 1, name: 'Lemon Drop', brand: "Dawg Star", product: "Flower", date:'01-01-2018'},
-      {id: 2, name: 'GG #4', brand: "Dawg Star", product: "Flower", date:'01-01-2018'},
-      {id: 3, name: 'LA Confidential', brand: "Dawg Star", product: "Flower", date:'01-02-2018'},
-      {id: 4, name: 'Frosted Flakes', brand: "Dawg Star", product: "Flower", date:'01-03-2018'},
-      {id: 5, name: 'Blueberry', brand: "Dawg Star", product: "Flower", date:'01-03-2018'},
-      {id: 6, name: 'Island Breeze', brand: "Western Cultured", product: "Flower", date:'01-03-2018'},
-      {id: 7, name: 'Kraken Black Pepper', brand: "Western Cultured", product: "Flower", date:'01-04-2018'},
-      {id: 8, name: 'PermaFrost', brand: "Western Cultured", product: "Flower", date:'01-04-2018'},
+      {name: 'Lemon Drop', brand: "Dawg Star", product: "Flower", date:'01-01-2018'},
+      { name: 'GG #4', brand: "Dawg Star", product: "Flower", date:'01-01-2018'},
+      {name: 'LA Confidential', brand: "Dawg Star", product: "Flower", date:'01-02-2018'},
+      {name: 'Frosted Flakes', brand: "Dawg Star", product: "Flower", date:'01-03-2018'},
+      // {id: 5, name: 'Blueberry', brand: "Dawg Star", product: "Flower", date:'01-03-2018'},
+      // {id: 6, name: 'Island Breeze', brand: "Western Cultured", product: "Flower", date:'01-03-2018'},
+      // {id: 7, name: 'Kraken Black Pepper', brand: "Western Cultured", product: "Flower", date:'01-04-2018'},
+      // {id: 8, name: 'PermaFrost', brand: "Western Cultured", product: "Flower", date:'01-04-2018'},
     ],
     newLog: {},
 }, action) {
 
   switch(action.type) {
     case "ADD_LOG": {
+      console.log('trying')
       return {
         ...state,
         logs: [...state.logs, action.payload],
