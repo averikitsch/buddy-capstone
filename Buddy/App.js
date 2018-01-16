9e399e31086be02c66b55eeb21af0c92e2fcba47
averikitsch <aekitsch@gmail.com>
2018-01-16 14:51:09 -0800

    start to authentication

--- Buddy/App.js
+++ Buddy/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View, StyleProvider } from 'react-native';
-// import Amplify, { withAuthenticator, Storage } from 'aws-amplify-react-native';
-// import aws_exports from './aws-exports';
-// Amplify.configure(aws_exports);
+// import { StyleSheet, Text, View, StyleProvider } from 'react-native';
+
+import Amplify, { withAuthenticator, Storage } from 'aws-amplify-react-native';
+import aws_exports from './aws-exports';
+Amplify.configure(aws_exports);
+
 import { TabNavigator, StackNavigator } from 'react-navigation';
-// import { Button, Footer, FooterTab } from 'native-base';
-// import Icon from 'react-native-vector-icons/Ionicons';
 import { Provider } from 'react-redux';
 import store from './src/store';
 import axios from 'axios';
@@ -15,7 +15,7 @@ import { fetchStrains } from './src/Actions/index';
 
 
 
-export default class Buddy extends React.Component {
+class Buddy extends React.Component {
   constructor() {
     super();
     axios.get(`http://strainapi.evanbusse.com/5QPNwCQ/strains/search/all`)
@@ -34,13 +34,4 @@ export default class Buddy extends React.Component {
   }
 }
 
-// export default withAuthenticator(App);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default withAuthenticator(Buddy);
